725b217ff21e525698e9ae3f10076893fa60553d
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-03-21 18:27:37 -0700

    added hook, css

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,32 +1,37 @@
+import React from 'react';
 import './Form.scss';
 
 const Form = (props) => {
+  
   const handleSubmit = e => {
+   
     e.preventDefault();
     const formData = {
-      method: 'GET',
-      url: 'https://pokeapi.co/api/v2/pokemon',
+      method: e.target.method.value || 'GET',
+      url: e.target.url.value,
     };
-    this.props.handleApiCall(formData);
+    props.handleApiCall(formData);
   }
-return (
-  <>
-    <form onSubmit={handleSubmit}>
-      <label >
-        <span>URL: </span>
-        <input name='url' type='text' />
-        <button type="submit">GO!</button>
-      </label>
-      <label className="methods">
-        <span id="get">GET</span>
-        <span id="post">POST</span>
-        <span id="put">PUT</span>
-        <span id="delete">DELETE</span>
-      </label>
-    </form>
-  </>
-);
-  }
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label >
+          <span>URL: </span>
+          <input name='url' type='text'/>
+          <button type="submit">GO!</button>
+        </label>
+        <label className="methods">
+          <select>
+            <option id="get">GET</option>
+            <option id="post">POST</option>
+            <option id="put">PUT</option>
+            <option id="delete">DELETE</option>
+          </select>
+        </label>
+      </form>
+    </>
+  );
+}
 
 
 export default Form;
